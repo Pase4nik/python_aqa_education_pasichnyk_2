d7dbb726258f26b6c859f191789aa501c56d4ed4
Pasichnyk <pase4nik.zhenya@gmail.com>
2021-11-21 14:31:28 +0200

    banking system done

--- banking_system/account.py
+++ banking_system/account.py
@@ -1,21 +1,87 @@
-class Account:
-    def __init__(self):
-        self.balance = 0
+import sqlite3
+from termcolor import cprint
+
+
+class Account(BaseException):
+    __CONNECTION = sqlite3.connect('card.s3db')
+    __CURSOR = __CONNECTION.cursor()
+
+    def __init__(self, data):
+        self.id = data.id
+        self.card = data.card
+        self.pin = data.pin
+        self.balance = data.balance
 
     def account_menu(self):
-        options = {1: 'Balance', 2: 'Log out', 0: 'Exit'}
-        actions = {1: self.show_balance, 2: self.log_out, 0: exit}
+        options = {1: 'Balance', 2: 'Add Income', 3: 'Do Transfer', 4: 'Close Account', 5: 'Log Out', 0: 'Exit'}
+        actions = {1: self.show_balance, 2: self.add_income, 3: self.do_transfer,
+                   4: self.close_account, 5: self.log_out, 0: exit}
         for i in options:
             print(f"{i}: {options.get(i)}", sep="\n")
-        user_choice = int(input('> '))
-        if user_choice not in options.keys():
-            print("This option does not exist.\nPlease try again")
-        else:
+        try:
+            user_choice = int(input('> '))
             actions[user_choice]()
+        except KeyError:
+            cprint("This option does not exist.\nPlease try again", "red")
+        return self.account_menu()
 
     def show_balance(self):
         print('Balance:', self.balance)
         return self.account_menu()
 
-    def log_out(self):
-        print("You have successfully logged out!")
+    def add_income(self):
+        print("Enter income:")
+        try:
+            income_amount = input("> ")
+            self.balance += income_amount
+            self.__CURSOR.execute(f'UPDATE CARD SET balance = {self.balance} WHERE id = {self.id}')
+            self.__CONNECTION.commit()
+            print('Income was added')
+        except TypeError:
+            cprint("Please, enter digits only", 'red')
+        return self.account_menu()
+
+    def do_transfer(self):
+        print("Transfer:", "Enter card number:", sep="\n")
+        card = input("> ")
+        if len(card) == 16 and card.isdigit():
+            if card != self.card:
+                other_id = self.get_account_id(card)
+                if other_id:
+                    other_id = other_id[0]
+                    print("Enter how much money you want to transfer:")
+                    amount = input("> ")
+                    if amount.isdigit() and int(amount) > 0:
+                        amount = int(amount)
+                        if amount <= self.balance:
+                            self.balance -= amount
+                            self.__CURSOR.execute(f'UPDATE CARD SET balance = {self.balance} WHERE id = {self.id}')
+                            self.__CURSOR.execute(f'UPDATE CARD SET balance = '
+                                                  f'balance + ({amount}) WHERE id = {other_id}')
+                            self.__CONNECTION.commit()
+                            cprint("\n Success!", 'green')
+                        else:
+                            cprint("\n Not enough money!", 'red')
+                    else:
+                        cprint("\n Incorrect amount!", 'red')
+                else:
+                    cprint("\n Such a card does not exist.", 'red')
+            else:
+                cprint("\n You can't transfer money to the same account!", 'red')
+        else:
+            cprint("\n Probably you made mistake in the card number. Please try again!", 'red')
+        return self.account_menu()
+
+    def get_account_id(self, card):
+        self.__CURSOR.execute(f'''SELECT id FROM CARD WHERE NUMBER = {card}''')
+        account_id = self.__CURSOR.fetchone()
+        return account_id
+
+    def close_account(self):
+        self.__CURSOR.execute(f'DELETE FROM CARD WHERE id = {self.id}')
+        cprint("The account has been closed!", 'green')
+        self.__CONNECTION.commit()
+
+    @staticmethod
+    def log_out():
+        cprint(" You have successfully logged out!", "green")
--- banking_system/bank.py
+++ banking_system/bank.py
@@ -1,37 +1,85 @@
 from account import Account
+from card import generate_card_number, generate_account_number, generate_pin
+from termcolor import cprint
+import sqlite3
+from collections import namedtuple
 
 
 class Bank:
+    Account = namedtuple('Account', 'id, card, pin, balance')
+    __connection = sqlite3.connect('card.s3db')
+    __cursor = __connection.cursor()
+
     def __init__(self):
-        pass
+        self.sql = self.__cursor.executescript('''CREATE TABLE IF NOT EXISTS CARD(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        number TEXT NOT NULL,
+        pin_code TEXT NOT NULL,
+        balance INTEGER NOT NULL DEFAULT 0
+        )''')
+        self.__connection.commit()
+        self.main_menu()
 
     def main_menu(self):
-        options = {1: 'Create an account', 2: 'Log into account', 0: 'Exit'}
-        actions = {1: self.new_account, 2: self.log_in, 0: exit}
+        options = {'1': 'Create an account', '2': 'Log into account', '0': 'Exit'}
+        actions = {'1': self.new_account, '2': self.log_in, '0': self.exit}
         for i in options:
             print(f"{i}: {options.get(i)}", sep="\n")
-        user_choice = int(input('> '))
-        if user_choice not in options.keys():
-            print("This option does not exist.\nPlease try again")
-        else:
+        try:
+            user_choice = input('> ')
             actions[user_choice]()
+        except KeyError:
+            cprint("This option does not exist.\nPlease try again", "red")
         return self.main_menu()
 
     def new_account(self):
-        print("\nYour card has been created")
+        cprint("\n Your card has been created", 'green')
+        accounts = self.get_all_accounts()
+        while True:
+            new_number = generate_account_number()
+            card = generate_card_number(new_number)
+            if card not in accounts.keys():
+                break
+        pin_code = generate_pin()
+        print("\nYour card number:", card, "Your card PIN:", pin_code, sep="\n")
+        self.__cursor.execute(f'''INSERT INTO CARD(number, pin_code) VALUES ({card}, {pin_code})''')
+        self.__connection.commit()
+
+    @staticmethod
+    def get_all_accounts():
+        accounts = {}
+        for acc in accounts:
+            accounts[acc.card] = acc.pin_code
+        return accounts
 
     def log_in(self):
-        print("Enter your card number:")
-        card_number = input("> ")
-        if card_number.isdigit():
+        self.__cursor.execute('''SELECT * from CARD''')
+        result = self.__cursor.fetchall()
+        print(result)
+        try:
+            print("Enter your card number:")
+            card_number = int(input("> "))
             print("Enter your PIN:")
-            pin_code = input(">").strip()
-            if pin_code.isdigit():
-                print("You have successfully logged in!")
-                account = Account()
+            pin_code = int(input())
+            account = self.get_account(card_number, pin_code)
+            if account:
+                account = Account(account)
+                cprint("\n You have successfully logged in!", "green")
                 account.account_menu()
                 return
-        print("Wrong card number or PIN!")
+            cprint("\n Wrong card number or PIN!", "red")
+        except ValueError:
+            cprint("\n Wrong card number or PIN!", "red")
+
+    def get_account(self, account_number, pin_code):
+        self.__cursor.execute(f'''SELECT * FROM CARD WHERE NUMBER = {account_number} AND pin_code = {pin_code}''')
+        f = self.__cursor.fetchone()
+        return self.Account._make(f) if f else None
+
+    def exit(self):
+        print("\nBye!")
+        self.__connection.close()
+        exit()
 
 
 def main():
